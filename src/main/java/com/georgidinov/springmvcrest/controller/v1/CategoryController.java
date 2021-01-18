dfb06d959e9c1c1f17fe3f5dbad8f9cf3941c77c
Georgi Dinov <georgidinov1@abv.bg>
2021-01-18 14:54:19 +0200

    Refactored CategoryController with rest controller annotation.

--- src/main/java/com/georgidinov/springmvcrest/controller/v1/CategoryController.java
+++ src/main/java/com/georgidinov/springmvcrest/controller/v1/CategoryController.java
@@ -5,15 +5,15 @@ import com.georgidinov.springmvcrest.api.v1.model.CategoryListDTO;
 import com.georgidinov.springmvcrest.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import static com.georgidinov.springmvcrest.util.ApplicationConstants.CATEGORIES_BASE_URL;
 
-@Controller
+@RestController
 @RequestMapping(CATEGORIES_BASE_URL)
 public class CategoryController {
 
@@ -25,16 +25,14 @@ public class CategoryController {
     }
 
     @GetMapping
-    public ResponseEntity<CategoryListDTO> getAllCategories() {
-        return new ResponseEntity<>
-                (
-                        new CategoryListDTO(categoryService.getAllCategories()),
-                        HttpStatus.OK
-                );
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryListDTO getAllCategories() {
+        return new CategoryListDTO(categoryService.getAllCategories());
     }
 
     @GetMapping("/{name}")
-    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name){
-        return new ResponseEntity<>(categoryService.getCategoryByName(name), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDTO getCategoryByName(@PathVariable String name) {
+        return categoryService.getCategoryByName(name);
     }
 }
